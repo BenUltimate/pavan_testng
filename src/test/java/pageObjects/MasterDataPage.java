25648a4aaeffb92fb7b4a38f28f80b036989407f
BenUltimate <jeevatirupathi111@gmail.com>
2025-06-03 18:27:05 +0530

    Came upto Master Data-> Contient Click

--- src/test/java/pageObjects/MasterDataPage.java
+++ src/test/java/pageObjects/MasterDataPage.java
@@ -2,13 +2,13 @@ package pageObjects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class MasterDataPage extends BasePage {
 
 	public MasterDataPage(WebDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
 	}
 
 	@FindBy(xpath = "//span[contains(@class,'sidenav-nav-item-span') and contains(., 'Master Data')]")
@@ -17,13 +17,44 @@ public class MasterDataPage extends BasePage {
 	@FindBy(xpath = "//button[normalize-space()='Continent']")
 	private WebElement continentButton;
 
+	@FindBy(id = "accountDropdownMenuLink")
+	private WebElement myAccount;
+
+	@FindBy(xpath = "//div[normalize-space()='My Dashboard']")
+	private WebElement dashboard;
+
+	// Actions
 	public void clickMasterDataMenu() {
 		masterDataMenu.click();
 	}
 
 	public void clickContinentButton() {
 		continentButton.click();
-		;
 	}
 
+	public void clickMyAccount() {
+		Actions act = new Actions(driver);
+		act.moveToElement(myAccount).click().build().perform();
+	}
+
+	public void clickDashboard() {
+		dashboard.click();
+	}
+
+	// Getters for waits
+	public WebElement getMyAccount() {
+		return myAccount;
+	}
+
+	public WebElement getDashboard() {
+		return dashboard;
+	}
+
+	public WebElement getMasterDataMenu() {
+		return masterDataMenu;
+	}
+
+	public WebElement getContinentButton() {
+		return continentButton;
+	}
 }
